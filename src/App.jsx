1433935a8dcfb6a13ce80e9f04e7101ca53373da
usmt-cop <usmt.cop@gmail.com>
2023-08-02 18:45:58 +0530

    learned props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
 
-function Search() {
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event)
+    console.log(event.target.value)
+  }
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" onChange={handleChange} type="text" />
     </div>
   )
 }
 
-function Item(object) {
+const Item = (object)=> {
   const item = object.item
   return (
     <div>
@@ -42,22 +28,40 @@ function Item(object) {
   )
 }
 
-function List() {
+const List = (prop)=> {
   return (
     <ul>
-    {list.map(function (item) {
-      return (
-        <li key={item.objectID}>
+      {prop.list.map((item) => {
+        return (
+          <li key={item.objectID}>
           <Item item={item} />
-        </li>
-      )
-    })}
-  </ul>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
-function App() {
-
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
+  
   return (
     <>
       <div>
@@ -65,7 +69,7 @@ function App() {
         <Search />
         <hr />
 
-        <List />
+        <List list={stories} />
       </div>
     </>
   )
