057830de7df796f0ffecc628ee54220b54845854
usmt-cop <usmt.cop@gmail.com>
2023-08-02 13:41:59 +0530

    updated list using components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
 
-const a = [1, 2, 3];
-const b = [...a, 4, 5, 6];
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  )
+}
+
+function List() {
+  return (
+    <ul>
+    {list.map(function (item) {
+      return <li key={item.objectID}>
+        <span>{item.title}</span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+      </li>;
+    })}
+  </ul>
+  )
+}
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>a = {a}</h1>
-        <h1>b = {b}</h1>
+        <h1>My Hacker Stories</h1>
+        <Search />
+        <hr />
+
+        <List />
       </div>
     </>
   )
